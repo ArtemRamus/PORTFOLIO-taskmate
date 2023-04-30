33caae63b22f461c76673fc71c18a06c46b83bab
ArtemRamus <artem.ramus@gmail.com>
2023-04-30 16:54:36 +0300

    adding generic base.html + about + contac

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -3,4 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 def todolist(request):
-    return render(request, 'todolist.html', {})
+    context = {
+        'welcome_text': 'Welcome to Todo List App.',
+        }
+    return render(request, 'todolist.html', context)
+
+def contact(request):
+    context = {
+        'contact_text': 'Welcome to Contact Page',
+        }
+    return render(request, 'contact.html', context)
+
+def about(request):
+    context = {
+        'about_text': 'Welcome to About Page',
+        }
+    return render(request, 'about.html', context)
