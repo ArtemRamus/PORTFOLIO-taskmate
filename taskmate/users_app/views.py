25616d0a289547b584d453b76ef912632c52d779
ArtemRamus <artem.ramus@gmail.com>
2023-05-11 14:31:30 +0300

    form - email field added, modified with crispy_forms

--- taskmate/users_app/views.py
+++ taskmate/users_app/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomCreationForm
 from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        register_form = UserCreationForm(request.POST)
+        register_form = CustomCreationForm(request.POST)
         if register_form.is_valid():
             register_form.save()
             messages.success(request, ("New User Account Created. Login to Get Started."))
             return redirect('register')
     else:
-        register_form = UserCreationForm()
+        register_form = CustomCreationForm()
     return render(request, 'register.html', {'register_form': register_form})
