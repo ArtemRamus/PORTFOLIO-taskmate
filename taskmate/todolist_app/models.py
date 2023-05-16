bf801af186672a5e8e879b484b922663229adca1
ArtemRamus <artem.ramus@gmail.com>
2023-05-16 07:52:01 +0300

    admin task string edited - owner added

--- taskmate/todolist_app/models.py
+++ taskmate/todolist_app/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class TaskList(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     task = models.CharField(max_length=300)
     done = models.BooleanField(default=False)
     
     def __str__(self):
-        return self.task + " - " + str(self.done)
+        if self.done:
+            task_status = "done"
+        else:
+            task_status = "pending"
+        return f"{self.task}, {task_status}, {str(self.owner)}"
